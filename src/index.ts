80e5d10f6b9beb5e36a588b45c9888cb75cee077
kiharakeito <kiharakeito@gmail.com>
2022-05-13 02:50:15 +0900

    add mode

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,11 @@ class HitAndBlow {
   private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   private answer: string[] = []
   private tryCount = 0
+  private mode: 'normal' | 'hard'
+
+  constructor(mode: 'normal' | 'hard') {
+    this.mode = mode
+  }
 
   setting() {
     const answerLength = 3
@@ -80,7 +85,7 @@ class HitAndBlow {
 
 // 
 ;(async() => {
-  const hitAndBlow = new HitAndBlow()
+  const hitAndBlow = new HitAndBlow('normal')
   hitAndBlow.setting()
   await hitAndBlow.play()
   hitAndBlow.end()

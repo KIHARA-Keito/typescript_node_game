bea11602e8532b86dac5f23eff702523fa69c1ce
kiharakeito <kiharakeito@gmail.com>
2022-05-17 23:03:32 +0900

    promptSelect refactoring

--- src/index.ts
+++ src/index.ts
@@ -19,18 +19,18 @@ const promptInput = async (text: string) => {
 }
 
 //
-const promptSelect = async (text: string, values: readonly string[]): Promise<string> => {
+const promptSelect = async <T extends string>(text: string, values: readonly T[]): Promise<T> => {
   printLine(`\n${text}`)
   values.forEach((value) => {
     printLine(`- ${value}`)
   })
   printLine(`> `, false)
 
-  const input = await readLine()
+  const input = (await readLine()) as T
   if (values.includes(input)) {
     return input
   } else {
-    return promptSelect(text, values)
+    return promptSelect<T>(text, values)
   }
 }
 
@@ -42,7 +42,7 @@ class HitAndBlow {
   private mode: Mode = 'normal'
 
   async setting() {
-    this.mode = await promptSelect('モードを入力してください', ['normal', 'hard']) as Mode
+    this.mode = await promptSelect<Mode>('モードを入力してください', ['normal', 'hard'])
     const answerLength = this.getAnswerLength()
 
     while (this.answer.length < answerLength) {
@@ -123,11 +123,4 @@ class HitAndBlow {
   await hitAndBlow.setting()
   await hitAndBlow.play()
   hitAndBlow.end()
-  /*
-  const name = await promptInput('名前を入力して下さい')
-  console.log(name)
-  const age = await promptInput('年齢を入力してください')
-  console.log(age)
-  process.exit()
-  */ 
 })()
